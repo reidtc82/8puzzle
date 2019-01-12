41369418006ec9bcd1aa4102e464c4440f8ff4e9
reidtc82 <reidtc82@gmail.com>
2019-01-12 11:51:17 -0800

    adding control buttons

--- PuzzleBoard.py
+++ PuzzleBoard.py
@@ -3,10 +3,15 @@ from Tile import Tile
 
 class PuzzleBoard:
     tiles = [0] *9
+    winningState = [0] *9
+
     def __init__(self):
         for i in range(9):
             self.tiles[i] = Tile(i)
+            self.winningState[i] = Tile(i)
+
         self.tiles = np.asarray(self.tiles).reshape(3, 3)
+        self.winningState = np.asarray(self.winningState).reshape(3,3)
 
     def getState(self):
         return self.tiles
@@ -20,3 +25,22 @@ class PuzzleBoard:
         for i in range(3):
             for j in range(3):
                 print(self.tiles[i][j].getValue())
+
+    def moveUp(self):
+        print('up')
+        return self.checkWin()
+
+    def moveDown(self):
+        print('down')
+        return self.checkWin()
+
+    def moveLeft(self):
+        print('left')
+        return self.checkWin()
+
+    def moveRight(self):
+        print('right')
+        return self.checkWin()
+
+    def checkWin(self):
+        return np.array_equal(self.tiles, self.winningState)
--- main.py
+++ main.py
@@ -10,19 +10,27 @@ class Main:
         master.title("8 Puzzle")
         master.geometry('640x480')
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(master, text="This is 8 Puzzle")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Reset Puzzle", command=self.board.resetPuzzle)
-        self.greet_button.pack()
+        self.resetPuzzle_button = Button(master, text="Reset Puzzle", command=self.board.resetPuzzle)
+        self.resetPuzzle_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
-    # def resetPuzzle(self):
-    #     self.board.resetState()
-    #     currentState = np.asarray(self.board.getState())
-    #     print(currentState[0].getValue())
+        self.up_button = Button(master, text="Up", command=self.board.moveUp)
+        self.up_button.pack()
+
+        self.down_button = Button(master, text="Down", command=self.board.moveDown)
+        self.down_button.pack()
+
+        self.left_button = Button(master, text="Left", command=self.board.moveLeft)
+        self.left_button.pack()
+
+        self.right_button = Button(master, text="Right", command=self.board.moveRight)
+        self.right_button.pack()
+
 
 root = Tk()
 mainPanel = Main(root)
