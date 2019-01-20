21d0531b869f17c51038f0f52e94fa23f9f78047
reidtc82 <reidtc82@gmail.com>
2019-01-19 22:28:06 -0800

    state wrok

--- State.py
+++ State.py
@@ -1,6 +1,20 @@
 class State:
     def __init__(self, state):
         self.state = state
+        self.cost = 0
+        self.path = []
 
     def getState(self):
         return self.state
+
+    def getCost(self):
+        return self.cost
+
+    def getPath(self):
+        return self.path
+
+    def addCost(self,pathCost):
+        self.cost += pathCost
+
+    def addPath(self,parent):
+        self.path.append(parent)
