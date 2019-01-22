c92310f9c8c6a497adc0eaf6b237c80c8d5e5c53
reidtc82 <reidtc82@gmail.com>
2019-01-21 22:58:19 -0800

    working on iterative deepening

--- Solver.py
+++ Solver.py
@@ -2,6 +2,338 @@ import numpy as np
 from State import State
 from copy import deepcopy
 from Direction import Direction
+import time
+import math
+
+class solver_iterative_deepening:
+    visited = set()
+    queue = []
+    queue_track = set()
+    pathTree = dict()
+    path = []
+
+    def __init__(self, startingState, goalState, isUniformCost):
+        self.queue.append(startingState)
+        self.queue_track.add(repr(startingState.getState()))
+        self.goalState = goalState
+        self.iUC = isUniformCost
+        self.pathTree[startingState] = {'parent':startingState.getParent(), 'cost':startingState.getCost()}
+        self.moves = 0
+        self.start_state = startingState
+
+    def solve(self):
+        t0 = time.time()
+        current_depth = 0
+
+        for i in range(0, 3):
+            while not self.is_empty(self.queue):
+                # print('at depth {0}'.format(current_depth))
+                if self.moves%1 == 0:
+                    print("Yes I'm still working current moves: {0} ".format(self.moves)+" current queue length: {0}".format(len(self.queue)))
+                # print('queue length before pop')
+                # print(len(self.queue))
+                currentState = self.queue.pop(0)
+                self.queue_track.remove(repr(currentState.getState()))
+                # print(currentState.getState())
+                # print('queue length after pop')
+                # print(len(self.queue))
+                self.visited.add(repr(currentState.getState()))
+                if np.allclose(self.goalState.getState(), currentState.getState()):
+                    print('***********end*************')
+                    self.returnPath(currentState)
+                    break
+                elif current_depth <= i:
+                    for child in self.successors(currentState):
+                        # print('visited')
+                        # print(self.visited)
+                        # print(self.check_visited(child))
+                        if not self.check_visited(child):
+                            # print('queue')
+                            # print(self.queue)
+                            if not self.check_queue(child):
+                                #store info from when created in successor funciton.
+                                # print('child')
+                                # print(child.getState())
+                                self.pathTree[child] = {'parent':child.getParent(), 'cost':child.getCost(), 'direction':child.getDirection()}
+                                #append because breadth first. Use insert for depth first.
+                                self.queue.insert(0, child)
+                                self.queue_track.add(repr(child.getState()))
+                        #ha I think one tab was messing me up
+                        # self.visited[currentState.getState()] = True
+
+                self.moves += 1
+                if self.moves > 10:
+                    t1 = time.time()
+                    print(t1-t0)
+                    break
+
+            #start over
+            self.queue.append(self.start_state)
+            self.queue_track.add(repr(self.start_state.getState()))
+            # self.goalState = goalState
+            # self.iUC = isUniformCost
+            self.pathTree[self.start_state] = {'parent':self.start_state.getParent(), 'cost':self.start_state.getCost()}
+            # self.moves = 0
+            current_depth = 0
+            self.visited.clear()
+
+    def check_visited(self, child):
+        result = False
+        # for v in self.visited:
+        #     if np.allclose(v.getState(), child.getState()):
+        #         result = True
+        #         break
+        if repr(child.getState()) in self.visited:
+            result = True
+        # try:
+        #     if self.visited[child.getState()]:
+        #         result = True
+        # except:
+        #     pass
+        return result
+
+    def check_queue(self, child):
+        result = False
+        # for q in self.queue:
+        #     if np.allclose(q.getState(), child.getState()):
+        #         result = True
+        #         break
+        if repr(child.getState()) in self.queue_track:
+            result = True
+        return result
+
+    def successors(self, root):
+        successors = []
+        zero_x = root.getZeroLocation()['col']
+        zero_y = root.getZeroLocation()['row']
+        temp = root.getState()#wtf python?! I couldnt just pass root.getState() to deepcopy, no, that didnt make a copy. I had to do it like this...
+
+        # dont forget to go back and deal with all these ridiculous variables
+        newLState = deepcopy(temp)
+        newRState = deepcopy(temp)
+        newUState = deepcopy(temp)
+        newDState = deepcopy(temp)
+        #inverted y axis
+
+        if zero_x != 0:
+            #take left child
+            leftTile = newLState[zero_x-1][zero_y]
+            newLState[zero_x-1][zero_y] = 0
+            newLState[zero_x][zero_y] = leftTile
+
+            cost = leftTile if self.iUC else 1
+            leftNew = State(newLState,cost,root,Direction.LEFT)
+
+            successors.insert(0,leftNew)
+
+        if zero_x != 2:
+            #take right child
+            rightTile = newRState[zero_x+1][zero_y]
+            newRState[zero_x+1][zero_y] = 0
+            newRState[zero_x][zero_y] = rightTile
+
+            cost = rightTile if self.iUC else 1
+            rightNew = State(newRState,cost,root,Direction.RIGHT)
+
+            successors.insert(0,rightNew)
+
+        if zero_y != 0:
+            #take upper child
+            upTile = newUState[zero_x][zero_y-1]
+            newUState[zero_x][zero_y-1] = 0
+            newUState[zero_x][zero_y] = upTile
+
+            cost = upTile if self.iUC else 1
+            upNew = State(newUState,cost,root,Direction.UP)
+
+            successors.insert(0,upNew)
+
+        if zero_y != 2:
+            #take lower child
+            downTile = newDState[zero_x][zero_y+1]
+            newDState[zero_x][zero_y+1] = 0
+            newDState[zero_x][zero_y] = downTile
+
+            cost = downTile if self.iUC else 1
+            downNew = State(newDState,cost,root,Direction.DOWN)
+
+            successors.insert(0,downNew)
+
+        return successors
+
+    def returnPath(self, node):
+        #returns list of dicts to use to define solution. Evrything should be in visited
+        # for n in self.pathTree:
+        #     print(self.pathTree[n])
+        # print(self.pathTree[node]['parent'])
+        while self.pathTree[node]['parent'] != []:
+            self.path.insert(0, {'node':node, 'data':self.pathTree[node]})
+            node = self.pathTree[node]['parent']
+
+    def get_path(self):
+        return self.path
+
+    def is_empty(self,ls):
+        return False if ls else True
+
+#----------------------------------------------------
+
+class solver_depthFirst:
+    visited = set()
+    queue = []
+    queue_track = set()
+    pathTree = dict()
+    path = []
+
+    def __init__(self, startingState, goalState, isUniformCost):
+        self.queue.append(startingState)
+        self.queue_track.add(repr(startingState.getState()))
+        self.goalState = goalState
+        self.iUC = isUniformCost
+        self.pathTree[startingState] = {'parent':startingState.getParent(), 'cost':startingState.getCost()}
+        self.moves = 0
+
+    def solve(self):
+        t0 = time.time()
+        while not self.is_empty(self.queue):
+            if self.moves%10000 == 0:
+                print("Yes I'm still working current moves: {0} ".format(self.moves)+" current queue length: {0}".format(len(self.queue)))
+            # print('queue length before pop')
+            # print(len(self.queue))
+            currentState = self.queue.pop(0)
+            self.queue_track.remove(repr(currentState.getState()))
+            # print(currentState.getState())
+            # print('queue length after pop')
+            # print(len(self.queue))
+            self.visited.add(repr(currentState.getState()))
+            if np.allclose(self.goalState.getState(), currentState.getState()):
+                print('***********end*************')
+                self.returnPath(currentState)
+                break
+            else:
+                for child in self.successors(currentState):
+                    # print('visited')
+                    # print(self.visited)
+                    # print(self.check_visited(child))
+                    if not self.check_visited(child):
+                        # print('queue')
+                        # print(self.queue)
+                        if not self.check_queue(child):
+                            #store info from when created in successor funciton.
+                            # print('child')
+                            # print(child.getState())
+                            self.pathTree[child] = {'parent':child.getParent(), 'cost':child.getCost(), 'direction':child.getDirection()}
+                            #append because breadth first. Use insert for depth first.
+                            self.queue.insert(0, child)
+                            self.queue_track.add(repr(child.getState()))
+                    #ha I think one tab was messing me up
+                    # self.visited[currentState.getState()] = True
+            self.moves += 1
+            if self.moves > 1000000:
+                t1 = time.time()
+                print(t1-t0)
+                break
+
+    def check_visited(self, child):
+        result = False
+        # for v in self.visited:
+        #     if np.allclose(v.getState(), child.getState()):
+        #         result = True
+        #         break
+        if repr(child.getState()) in self.visited:
+            result = True
+        # try:
+        #     if self.visited[child.getState()]:
+        #         result = True
+        # except:
+        #     pass
+        return result
+
+    def check_queue(self, child):
+        result = False
+        # for q in self.queue:
+        #     if np.allclose(q.getState(), child.getState()):
+        #         result = True
+        #         break
+        if repr(child.getState()) in self.queue_track:
+            result = True
+        return result
+
+    def successors(self, root):
+        successors = []
+        zero_x = root.getZeroLocation()['col']
+        zero_y = root.getZeroLocation()['row']
+        temp = root.getState()#wtf python?! I couldnt just pass root.getState() to deepcopy, no, that didnt make a copy. I had to do it like this...
+
+        # dont forget to go back and deal with all these ridiculous variables
+        newLState = deepcopy(temp)
+        newRState = deepcopy(temp)
+        newUState = deepcopy(temp)
+        newDState = deepcopy(temp)
+        #inverted y axis
+
+        if zero_x != 0:
+            #take left child
+            leftTile = newLState[zero_x-1][zero_y]
+            newLState[zero_x-1][zero_y] = 0
+            newLState[zero_x][zero_y] = leftTile
+
+            cost = leftTile if self.iUC else 1
+            leftNew = State(newLState,cost,root,Direction.LEFT)
+
+            successors.insert(0,leftNew)
+
+        if zero_x != 2:
+            #take right child
+            rightTile = newRState[zero_x+1][zero_y]
+            newRState[zero_x+1][zero_y] = 0
+            newRState[zero_x][zero_y] = rightTile
+
+            cost = rightTile if self.iUC else 1
+            rightNew = State(newRState,cost,root,Direction.RIGHT)
+
+            successors.insert(0,rightNew)
+
+        if zero_y != 0:
+            #take upper child
+            upTile = newUState[zero_x][zero_y-1]
+            newUState[zero_x][zero_y-1] = 0
+            newUState[zero_x][zero_y] = upTile
+
+            cost = upTile if self.iUC else 1
+            upNew = State(newUState,cost,root,Direction.UP)
+
+            successors.insert(0,upNew)
+
+        if zero_y != 2:
+            #take lower child
+            downTile = newDState[zero_x][zero_y+1]
+            newDState[zero_x][zero_y+1] = 0
+            newDState[zero_x][zero_y] = downTile
+
+            cost = downTile if self.iUC else 1
+            downNew = State(newDState,cost,root,Direction.DOWN)
+
+            successors.insert(0,downNew)
+
+        return successors
+
+    def returnPath(self, node):
+        #returns list of dicts to use to define solution. Evrything should be in visited
+        # for n in self.pathTree:
+        #     print(self.pathTree[n])
+        # print(self.pathTree[node]['parent'])
+        while self.pathTree[node]['parent'] != []:
+            self.path.insert(0, {'node':node, 'data':self.pathTree[node]})
+            node = self.pathTree[node]['parent']
+
+    def get_path(self):
+        return self.path
+
+    def is_empty(self,ls):
+        return False if ls else True
+
+#----------------------------------------------------
 
 class solver_breadthFirst:
     visited = []
--- SolverTester.py
+++ SolverTester.py
@@ -1,23 +1,40 @@
 from State import State
-from Solver import solver_breadthFirst
+from Solver import solver_breadthFirst, solver_depthFirst, solver_iterative_deepening
 from copy import deepcopy
 
 easyStart = State([[1,8,7],[3,6,0],[4,2,5]],0,[],None)
-what = State([[1,8,7],[0,2,6],[3,4,5]],0,[],None)
+what = State([[1,0,7],[2,8,6],[3,4,5]],0,[],None)
 winningState = State([[1,8,7],[2,0,6],[3,4,5]],0,[],None)
 
-solver = solver_breadthFirst(easyStart,winningState,False)
-solver.solve()
-solution = solver.get_path()
+# Set final arg to True to do Uniform-Cost
+# b_solver = solver_breadthFirst(easyStart,winningState,False)
+# b_solver.solve()
+# solution = b_solver.get_path()
+#
+# # test = [deepcopy(easyStart.getState()),deepcopy(winningState.getState())]
+# # print(deepcopy(easyStart.getState()) not in test)
+# for sol in solution:
+#     solNode = sol['data']
+#     if 'direction' in solNode.keys():
+#         print('Move '+str(solNode['direction']))
+#     else:
+#         print('Starting Breadth First')
+#     print('Cost is ')
+#     print(solNode['cost'])
+#     print('State')
+#     print(sol['node'].getState())
 
-# test = [deepcopy(easyStart.getState()),deepcopy(winningState.getState())]
-# print(deepcopy(easyStart.getState()) not in test)
-for sol in solution:
+
+d_solver = solver_iterative_deepening(easyStart,winningState,False)
+d_solver.solve()
+d_solution = d_solver.get_path()
+
+for sol in d_solution:
     solNode = sol['data']
     if 'direction' in solNode.keys():
         print('Move '+str(solNode['direction']))
     else:
-        print('Starting')
+        print('Starting Depth First')
     print('Cost is ')
     print(solNode['cost'])
     print('State')
